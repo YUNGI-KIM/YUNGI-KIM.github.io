9c94bdeea734570e1759abe2a122195f5aac749a
YUNGI-KIM <kimyungi0101@kakao.com>
2025-12-10 04:39:00 +0900

    initial

---
+++ src/config.ts
@@ -0,0 +1,46 @@
+export const siteConfig = {
+  name: "김윤기",
+  title: "HKNU. Department of Computer Engineering",
+  description: "Portfolio website of 김윤기",
+  accentColor: "#1d4ed8",
+  social: {
+    email: "kimyungi0101@hknu.ac.kr",
+    github: "https://github.com/YUNGI-KIM",
+  },
+  aboutMe:
+    "2nd year student at Hankyong National University, Department of Computer Engineering",
+  skills: ["Javascript", "React", "Node.js", "Python", "AWS", "Docker", "Java", "Spring Boot"],
+  projects: [
+    {
+      name: "Air Pollution Inquiry Mashup",
+      description:
+        "원하는 지역의 공기질을 측정하여 보기 쉽게 나타내는 어플리케이션입니다.",
+      link: "https://symphonious-tapioca-5138fb.netlify.app",
+      skills: ["HTML", "Javascript"],
+    },
+    {
+      name: "서울시 문화행사 알리미",
+      description:
+        "서울시에 어떤 문화행사가 언제 있는지 한번에 알아 볼 수 있는 어플리케이션입니다.",
+      link: "https://sparkling-swan-9c865c.netlify.app/",
+      skills: ["HTML", "Javascript"],
+    },
+    {
+      name: "Kakao Map을 이용한 서울시 공공도서관 검색 Mashup",
+      description:
+        "kakao map을 이용하여 서울시의 공공도서관을 검색하고 그 도서관에 대한 정보를 설명하는 어플리케이션입니다.",
+      link: "https://voluble-pony-9a71e4.netlify.app",
+      skills: ["HTML", "Javascript"],
+    },
+  ],
+  education: [
+    {
+      school: "Hankyong National University.",
+      degree: "Department of Computer Engineering",
+      dateRange: "2022 - Present",
+      achievements: [
+        "KSC 2025 우수논문상",
+      ],
+    },
+  ],
+};
